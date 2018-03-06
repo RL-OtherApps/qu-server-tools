3bf8e64f5dfdab2750f897ca2f2335765940fd37
xavierjimenez <xavier.jimenez@qubiq.es>
2018-03-06 17:56:58 +0100

    [FIX] disable_vat_check adapted to OCA format

--- disable_vat_check/__manifest__.py
+++ disable_vat_check/__manifest__.py
@@ -3,17 +3,15 @@
 # Copyright (c) 2017 QubiQ (http://www.qubiq.es)
 
 {
-    'name': 'disable VAT CHECK',
+    'name': 'Disable VAT Check',
     'version': '10.0.1.0.0',
     'category': 'Custom',
     'description': """
-        Disables the validation of TIN.
+        Remove VAT Check for partner importations.
     """,
     'author': 'QubiQ',
     'website': 'https://www.qubiq.es',
-    'depends': [
-                'base_vat'
-                ],
+    'depends': ['base_vat'],
     'data': [
             ],
     'auto_install': False,
--- disable_vat_check/models/__init__.py
+++ disable_vat_check/models/__init__.py
@@ -2,4 +2,4 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # Copyright (c) 2017 QubiQ (http://www.qubiq.es)
 
-import res_partner
+from . import res_partner
